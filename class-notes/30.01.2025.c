ccbb99c2a57f7c18d0b5a5438c4ee6c7d7fd4793
Mane <mane_hambardzumyan@edu.aua.am>
2025-03-13 22:17:55 +0400

    changes done

---
+++ class-notes/30.01.2025.c
@@ -0,0 +1,9 @@
+
+
+ #include <stdio.h>  
+#include <stdlib.h>  
+
+int main() {  
+    printf(" In the last class, we focused on data representation, particularly numeric systems. We explored the decimal system, which is the standard base-10 system we use in daily life. The class also delved into binary, a base-2 system that computers use to process information. Additionally, we covered hexadecimal, a base-16 system commonly used in programming and digital electronics. The lecture provided a deeper understanding of how these numeric systems are applied in computing and data storage.\n");  
+    return 0;  
+}
