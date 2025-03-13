189d3c12ef74d7c69fe0a7ab191ba3758a5bfef5
Mane <mane_hambardzumyan@edu.aua.am>
2025-03-13 22:14:43 +0400

    file added

---
+++ class-notes/24.01.2025.c
@@ -0,0 +1,13 @@
+#include(stdio.h)
+#include(stdlib.h)
+
+int main(){
+
+
+
+printf ("In class, we learned about UNIX and other operating systems, exploring their architecture, file systems, and command-line interfaces. We also discussed process management, user permissions, and the differences between various OS types.");
+
+
+returen 0;
+
+}
